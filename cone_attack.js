4311aeb291eb7537bc4b52999c26182a6c07f2d3
FelipeCampbell <fcampbell@uc.cl>
2022-04-10 19:52:57 -0400

    feat(collisions): check for scoops collisions

--- cone_attack.js
+++ cone_attack.js
@@ -3,6 +3,36 @@ const cones = [0, 1, 2, 3, 4, 5, 6, 7];
 const coneOptions = ['icecream_01', 'icecream_02', 'icecream_03'];
 let count = 0;
 
+const checkHorCollision = (ballStyle, playerPos) => {
+  const leftBall = parseInt(ballStyle.left.replace("px", ""));
+  return leftBall < playerPos.left + 65 && playerPos.left < leftBall + 60
+    ? true
+    : false;
+};
+
+const checkVertCollision = (ballStyle, playerPos) => {
+  const bottom = parseInt(ballStyle.bottom.replace("px", ""));
+  const bottomPlayer = 700 - 80 - playerPos.y;
+  return bottom < playerPos.bot + 80 && playerPos.bot < bottom + 80
+    ? true
+    : false;
+};
+
+const getPlayersPositions = () => {
+  const playerOneStyle = document.getElementById("player-one").style;
+  const playerTwoStyle = document.getElementById("player-two").style;
+  return {
+    one: parsePlayerPositions(playerOneStyle),
+    two: parsePlayerPositions(playerTwoStyle),
+  };
+};
+
+const parsePlayerPositions = (playerStyle) => {
+  const leftPlayer = parseInt(playerStyle.left.replace("px", "")) || 0;
+  const topPlayer = parseInt(playerStyle.top.replace("px", "")) || 0;
+  return { left: leftPlayer, bot: 700 - 80 - topPlayer };
+};
+
 rxjs.timer(0, 2000).subscribe((done) => {
   const element = document.createElement('div');
   var cone = cones[Math.floor(Math.random() * cones.length)];
@@ -37,4 +67,21 @@ rxjs.timer(0, 100).subscribe((done) => {
       document.getElementById('map').removeChild(elem);
     }
   });
+});
+
+rxjs.timer(0, 200).subscribe(() => {
+  const players = getPlayersPositions();
+  const ballsCollissioned = icecreamBalls.some((ballId) => {
+    const ballStyle = document.getElementById(ballId).style;
+    const playerOneCollision =
+      checkHorCollision(ballStyle, players.one) &&
+      checkVertCollision(ballStyle, players.one);
+    const playerTwoCollision =
+      checkHorCollision(ballStyle, players.two) &&
+      checkVertCollision(ballStyle, players.two);
+
+    return playerOneCollision || playerTwoCollision;
+  });
+
+  console.log(ballsCollissioned);
 });
